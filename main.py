fb3052907f576ea52d23611e7ee17ed7c5d4168f
Mar Joseph Bijer <70209257+Zethkiel@users.noreply.github.com>
2025-06-02 23:36:34 +0000

    updated interface

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from beneficiary_tracker.beneficiary import Beneficiary
 from beneficiary_tracker.beneficiary_manager import BeneficiaryManager
 
-print("""
+def main_menu():
+    while True:
+      print("""
 Welcome to the Food Bank System!
 1. Manage Inventory
 2. Manage Beneficiaries
 3. Distribute Food
 4. Exit Program
-      """)
+            """)
+      option = input("Please choose an option: ")
+
+      if option == "1":
+         print("Chose 1")
+         inventory_menu()
+      elif option == "2":
+         print("Chose 2")
+      elif option == "3":
+         print("Chose 3")
+      elif option == "4":
+         print("Exiting program. Goodbye!")
+         break
+      else:
+         print("That's not a valid option!")
+
+def inventory_menu():
+   while True:
+      print("Inventory Menu")
+      # more to be added
+      
+      option = input("Please choose an option: ")
+
+def beneficiary_menu():
+   while True:
+      print("Beneficiary Menu")
+      # more to be added
+
+      option = input("Please choose an option: ")
+
+main_menu()
