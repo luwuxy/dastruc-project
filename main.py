335839075ddfc5a7604b321dfd70e3c8eaf587c9
Zethkiel <marjosephbijer123@gmail.com>
2025-06-02 20:32:11 +0800

    sample interface

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello world!")
+from beneficiary_tracker.beneficiary import Beneficiary
+from beneficiary_tracker.beneficiary_manager import BeneficiaryManager
 
-
-print("ashley here")
+print("""
+Welcome to the Food Bank System!
+1. Manage Inventory
+2. Manage Beneficiaries
+3. Distribute Food
+4. Exit Program
+      """)
